54ef8f4f4f76fb001887518821021dda380eca81
lian <lu@asu.edu>
2016-10-18 16:41:20 -0500

    inital

---
+++ AcmeApp/Acme.Biz/Product.cs
@@ -0,0 +1,81 @@
+﻿using Acme.Common;
+using static Acme.Common.LoggingService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz
+{
+    /// <summary>
+    /// Manages products carried in inventory
+    /// </summary>
+    public class Product
+    {
+        public Product()
+        {
+            Console.WriteLine("Constructor invoke");
+        }
+
+        public Product(int productId,
+                       string productName,
+                       string description) :this()
+        {
+            Console.WriteLine("New Constructor invoke");
+            this.productId = productId;
+            this.description = description;
+            this.productName = productName;
+        }
+
+        private string productName;
+
+        public string ProductName
+        {
+            get { return productName; }
+            set { productName = value; }
+        }
+
+        private string description;
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        private int productId;
+
+        public int ProductId
+        {
+            get { return productId; }
+            set { productId = value; }
+        }
+
+        private Vendor productVender;
+
+        public Vendor ProductVender
+        {
+            get {
+                if (this.productVender == null) {
+                    this.productVender = new Vendor();
+                }
+                return productVender;
+            }
+            set { productVender = value; }
+        }
+
+
+        public string SayHello() {
+            //var vendor = new Vendor();
+            //vendor.SendWelcomeEmail("Message from Product");
+
+
+            var emailService = new EmailService();
+            var confirmation = emailService.SendMessage("New Product", this.productName, "sales@abc.com");
+            var result = LogAction("Saying Hello");
+            return "Hello " + ProductName + " ("+ProductId+"): "+Description;
+        }
+
+    }
+}
---
+++ AcmeApp/Tests/Acme.BizTests/ProductTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Acme.Biz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz.Tests
+{
+    [TestClass()]
+    public class ProductTests
+    {
+        [TestMethod()]
+        public void SayHelloTest()
+        {
+            var curProduct = new Product();
+            curProduct.ProductName = "Saw";
+            curProduct.ProductId = 1;
+            curProduct.Description = "gg";
+            curProduct.ProductVender.CompanyName = "ABC Corp";
+            var expect = "Hello Saw (1): gg";
+            Assert.AreEqual(expect, curProduct.SayHello());
+        }
+
+        [TestMethod()]
+        public void SayHelloTestParaConstructor()
+        {
+            var curProduct = new Product(1,"Saw","gg");
+            var expect = "Hello Saw (1): gg";
+            Assert.AreEqual(expect, curProduct.SayHello());
+        }
+
+        [TestMethod()]
+        public void SayHelloTest_null()
+        {
+            Product curProduct = null;
+            var companyName = curProduct?.ProductName;
+            var expect = "Hello Saw (1): gg";
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
